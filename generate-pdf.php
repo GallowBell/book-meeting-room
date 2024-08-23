72399f12fe9ddf62f225db1137ea4587f98e30d7
Prapon Pumsiri <Prapon.P@mithmitree.local>
2024-08-23 11:30:08 +0700

    example of generate-pdf

--- generate-pdf.php
+++ generate-pdf.php
@@ -5,24 +5,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 // Create an instance of the class:
 $mpdf = new \Mpdf\Mpdf();
 
-$html = "<table border='0'>
-    <tr>
-        <td>
-            TEST1
-        </td>
-        <td>
-            TEST2
-        </td>
-    </tr>
-    <tr>
-        <td>
-            TEST3
-        </td>
-        <td>
-            TEST4
-        </td>
-    </tr>
-</table>";
+$html = "<h1>Meeting Room Booking Confirmation</h1>
+<p><strong>Meeting Room:</strong> Conference Room A</p>
+<p><strong>Date:</strong> 2023-10-15</p>
+<p><strong>Time:</strong> 10:00 AM - 11:00 AM</p>
+<p><strong>Attendees:</strong></p>
+<ul>
+    <li>John Doe</li>
+    <li>Jane Smith</li>
+    <li>Michael Brown</li>
+</ul>";
 
 // Write some HTML code:
 $mpdf->WriteHTML($html);

89a6b56fb9872e36dec6083e40bc3742a40d1eb7
Prapon <vin_land@hotmail.com>
2024-08-23 09:23:14 +0700

    Optimize approve.php to using function to update database

--- approve.php
+++ approve.php
@@ -4,49 +4,71 @@
     header('Content-Type: application/json; charset=UTF-8');
 
     $status = isset($_POST['status']) ? $_POST['status'] : '';
-    
+
+
+    /**
+     * function for update status table reservations 
+     * @param array - parameter for update status is_approve of table reservations
+     * * status - 1 = approve, 0 = reject
+     * * reservation_id - primary of reservations to update record
+     */
+    function UpdateIsApprove($parameter = []){
+        global $conn;
+        $status = $parameter['status'];
+        $reservation_id = $parameter['reservation_id'];
+        $stmt = $conn->prepare("UPDATE reservations SET `is_approve`=? WHERE reservation_id=?");
+        $stmt->bind_param('si', $status, $reservation_id);
+        $QueryResult = $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        $message = 'ไม่อนุมัติการจอง';
+        if($status == 1){
+            $message = 'อนุมัติการจอง';
+        }
+        if($QueryResult){
+            // success
+            return [
+                'status' => 200,
+                'message' => $message.'เรียบร้อยแล้ว'
+            ];
+        }
+        // error
+        return [
+            'status' => 500,
+            'message' => 'เกิดข้อผิดพลาดในการ'.$message.' โปรดลองใหม่อีกครั้ง'
+        ];
+        
+    }
+
+    // อนุมัติ
     if($status == 'approve'){
         $reservation_id = $_POST['reservation_id'];
         $status = 1;
-        $stmt = $conn->prepare("UPDATE reservations SET `is_approve`=? WHERE reservation_id=?");
-        $stmt->bind_param('si', $status, $reservation_id);
-        $result = $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        if($result){
-            echo json_encode([
-                'status' => 200,
-                'message' => 'อนุมัติการจองเรียบร้อยแล้ว'
-            ]);
-        }else{
-            echo json_encode([
-                'status' => 500,
-                'message' => 'เกิดข้อผิดพลาดในการอนุมัติการจอง โปรดลองใหม่อีกครั้ง'
-            ]);
-        }
+        $result = UpdateIsApprove([
+            'reservation_id' => $reservation_id,
+            'status' => $status
+        ]);
+        echo json_encode($result);
         return;
     }
 
+    // ไม่อนุมัติ
     if ($status == 'reject') {
         $reservation_id = $_POST['reservation_id'];
         $status = 0;
-        $stmt = $conn->prepare("UPDATE reservations SET `is_approve`=? WHERE reservation_id=?");
-        $stmt->bind_param('si', $status, $reservation_id);
-        $result = $stmt->execute();
-        $stmt->close();
-        $conn->close();
-        if($result){
-            echo json_encode([
-                'status' => 200,
-                'message' => 'ไม่อนุมัติการจองเรียบร้อยแล้ว'
-            ]);
-        }else{
-            echo json_encode([
-                'status' => 500,
-                'message' => 'เกิดข้อผิดพลาดในการไม่อนุมัติการจองเรียบ โปรดลองใหม่อีกครั้ง'
-            ]);
-        }
+        $result = UpdateIsApprove([
+            'reservation_id' => $reservation_id,
+            'status' => $status
+        ]);
+        echo json_encode($result);
         return;
     }
+
+    echo json_encode([
+        'status' => 400,
+        'message' => 'Bad Request'
+    ]);
+
+    return;
 
 ?>

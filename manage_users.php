42fb743ad675f74eb5c3a7520da660e1777e289b
Prapon Pumsiri <Prapon.P@mithmitree.local>
2024-09-13 15:33:34 +0700

    remove datatable

--- manage_users.php
+++ manage_users.php
@@ -219,8 +219,27 @@ $roles = mysqli_query($conn, "SELECT * FROM roles");
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 <?php
-  include 'footer.php';
+  /* include 'footer.php'; */
   ?>
+  
+     <!-- ======= Footer ======= -->
+     <footer id="footer-full" class="footer">
+    <div class="credits">
+      ออกแบบและพัฒนาโดย ทีมนักศึกษาสหกิจ 2567 สาขาวิชาระบบสารสนเทศ <a href="https://bus.rmutp.ac.th/" target="_blank">คณะบริหารธุรกิจ</a>
+      <a href="https://www.rmutp.ac.th/" target="_blank"><p class="text-center">มหาวิทยาลัยเทคโนโลยีราชมงคลพระนคร (RMUTP)</p></a>
+    </div>
+  </footer><!-- End Footer -->
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.js"></script>
+  <!-- <script src="assets/vendor/simple-datatables/simple-datatables.js"></script> -->
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
 
 </body>
 
